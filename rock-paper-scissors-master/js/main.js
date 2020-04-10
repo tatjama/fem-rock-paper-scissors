3fa6e0eb92d366304d6faa486988747989fbaff1
tatjama <tanja120a@gmail.com>
2020-04-10 20:05:29 +0200

    start game

---
+++ rock-paper-scissors-master/js/main.js
@@ -0,0 +1,10 @@
+function showRules(){
+    document.querySelector(".rules").style.display = "inherit";
+
+  }
+  function hideRules(){
+    document.querySelector(".rules").style.display = "none";
+  }
+  function startGame(){
+      document.querySelector('.second').style.display = "none";
+  }
