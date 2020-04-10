67d4b0ca920025731c09f40ca72acae9cbab002f
tatjama <tanja120a@gmail.com>
2020-04-11 00:44:24 +0200

    Choose player card

--- rock-paper-scissors-master/js/main.js
+++ rock-paper-scissors-master/js/main.js
@@ -5,12 +5,15 @@ function showRules(){
   function hideRules(){
     document.querySelector(".rules").style.display = "none";
   }
-  function startGame(){
+  function pickCard(e){
+      var x = e.className;
+      console.log(x);
       document.querySelector('.second').style.display = "none";
       document.querySelector('.start-game').style.display = "initial";
+      document.getElementById('player-card').className = x;
   }
   function pick(){
       document.querySelector('#blank').style.display = "none";
-      document.querySelector('#house').style.display = "initial";
-      document.querySelector('#house').style.display = "flex";
+      document.querySelector('#house-card').style.display = "initial";
+      document.querySelector('#house-card').style.display = "flex";
   }
