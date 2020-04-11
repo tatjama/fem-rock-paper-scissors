bc58619bf7b72aa5a925c045e75f08cea33e2547
tatjama <tanja120a@gmail.com>
2020-04-11 07:01:09 +0200

    Show House card rundomly method

--- rock-paper-scissors-master/js/main.js
+++ rock-paper-scissors-master/js/main.js
@@ -10,14 +10,34 @@ function showRules(){
       console.log(x);
       y = x.slice(5);
       console.log(y);
-      document.getElementById('player-card-image').src = './images/icon-'+y+'.svg';
+      document.getElementById('player-card-image').src = './images/icon-'+ y +'.svg';
       document.querySelector('.second').style.display = "none";
       document.querySelector('.start-game').style.display = "initial";
       document.getElementById('player-card').className = x;
       
   }
-  function pick(){
+  function pickHouse(){
       document.querySelector('#blank').style.display = "none";
       document.querySelector('#house-card').style.display = "initial";
       document.querySelector('#house-card').style.display = "flex";
+      showHouseCard();
+      
+  }
+  function showHouseCard(){
+      let x = Math.floor((Math.random() * 3) + 1);    
+      let y = "";
+      switch(x){
+        case 1: 
+        y = 'paper';
+        break;
+        case 2: 
+        y = 'scissors';        
+        break;
+        case 3: 
+        y = 'rock';
+        break;
+      }
+      document.getElementById('house-card-image').src = './images/icon-' + y + '.svg';
+      document.getElementById('house-card').className = 'icon ' + y;
+
   }
