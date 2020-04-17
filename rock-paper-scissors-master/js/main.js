5e400b6662d784a0dedecac35d479196be8edaa9
tatjama <tanja120a@gmail.com>
2020-04-17 20:34:34 +0200

    game logic changed

--- rock-paper-scissors-master/js/main.js
+++ rock-paper-scissors-master/js/main.js
@@ -1,11 +1,17 @@
-    var paper = 'paper';
-     var rock = 'rock';
-     var scissors = 'scissors';
-     var spock = 'spock';
-     var lizard = 'lizard';
-     var messageWin = 'Y o u &nbsp W i n';
-     var messageLose = 'Y o u &nbsp L o s e';
-     var messageEqual = 'E q u a l';
+
+     var paper, rock, scissors, lizard, spock, equal, lose, win, messageEqual, messageLose, messageWin;     
+     
+      paper = 'paper';
+      rock =  'rock';
+      scissors = 'scissors';
+      spock = 'spock';
+      lizard = 'lizard';
+      equal = 'equal';
+      lose = 'lose';
+      win = 'win';
+      messageEqual = 'E q u a l';
+      messageWin = 'Y o u &nbsp W i n';
+      messageLose = 'Y o u &nbsp L o s e';
 
      var rulesOfGame = {
       paper: [rock, spock],
@@ -14,31 +20,16 @@
       spock: [scissors, rock],
       scissors: [paper, lizard]
     }
-    var localNameSerbia = {
-      localNameOfPaper :'papir',
-      localNameOfRock :'kamen',
-      localNameOfScissors : 'makaze',
-      localNameOfSpock : 'spok',
-      localNameOfLizard : 'guster'
-    }
-
-     var paper = localNameOfPaper;
-     var rock = localNameOfRock;
-     var scissors = localNameOfScissors;
-     var spock = localNameOfSpock;
-     var lizard = localNameOfLizard;
-     
      
 /*****START SCORE ONLOAD. SESSION VALUE IS START SCORE */
 function storageScore(){
-  // alert(sessionStorage.getItem('score')); 
-    
     if( isNaN(parseInt( sessionStorage.getItem('score') ))){
        document.querySelector('.score h1').innerHTML = 0;
      }else{
        document.querySelector('.score h1').innerHTML = parseInt(sessionStorage.getItem('score'));
      }
  }
+
 /*****RULES SHOW AND HIDE */
 function showRules(){
     document.querySelector(".rules").style.display = "inherit";
@@ -50,9 +41,7 @@ function showRules(){
  /*******GAME LOGIC , pick player card and bonus player card*/ 
   function pickCardPlayer(e, b, c, d, g){
       var x = e.className;
-      //console.log(x);
       y = x.slice(b);
-      //console.log(y);
       document.getElementById('player-card-image').src = './images/icon-'+ y +'.svg';
       document.querySelector(g).style.display = "none";
       document.querySelector('.start-game').style.display = "initial";
@@ -65,14 +54,13 @@ function showRules(){
       document.querySelector('#blank').style.display = "none";
       document.querySelector('#house-card').style.display = "flex";
       showHouseCard(3);
-      //console.log(rules());
      setTimeout(showResult, 1000);     
   }
+
   function pickCardHouseBonus(){  
     document.querySelector('#blank').style.display = "none";
     document.querySelector('#house-card').style.display = "flex";
     showHouseCard(5);
-    //console.log(rules());
     setTimeout(showResult, 1000); 
   }
 
@@ -81,19 +69,19 @@ function showRules(){
     let y = "";
     switch(x){
       case 1: 
-      y = paper;
+      y = 'paper';
       break;
       case 2: 
-      y = scissors;        
+      y = 'scissors';        
       break;
       case 3: 
-      y = rock;
+      y = 'rock';
       break;
       case 4:
-      y = spock;
+      y = 'spock';
       break;
       case 5:
-      y = lizard;
+      y = 'lizard';
       break;  
     }
     document.getElementById('house-card-image').src = './images/icon-' + y + '.svg';
@@ -101,23 +89,20 @@ function showRules(){
   }  
 
   /****GAME LOGIC */
-
   function gameLogic(){
-    let card1 = document.getElementById('player-card').className.slice(5);
-    let card2 = document.getElementById('house-card').className.slice(5);
-    let key = rulesOfGame[card1];
-    let value = card2; 
-
-    if(card1 === card2){
-      return 0;
-    }
-    for(let i = 0; i < key.length; i++ ){
-      if(key[i] == value ){
-         return 1;        
-      }else{
-        return -1;
-      }      
-    }    
+    let player = document.getElementById('player-card').className.slice(5);
+    let house = document.getElementById('house-card').className.slice(5);
+    let key = rulesOfGame[player];  
+    console.log(key);
+    if(player === house){
+      return equal;
+    }  
+    for(let i = 0; i < key.length; i++ ){        
+      if(key[i] == house ){        
+       return  win;        
+      }
+    }  
+    return lose;  
   }
    
   /****GAME RESULT SHOW AND SAVE IN SESSION - SAME FUNCTION IN BONUS*/
@@ -133,13 +118,13 @@ function showRules(){
     document.getElementById('play-again').style.display = 'flex';
     console.log(gameLogic())
     switch(gameLogic()){      
-      case 1:      
+      case win:      
       document.querySelector('.gradient').id = 'gradient';      
       document.querySelector('#play-again h2').innerHTML = messageWin;
       document.querySelector('.score h1').innerHTML = parseInt(sessionStorage.getItem('score')) + 1;
       document.querySelector('.gradient').style.display = 'initial';
       break;
-      case -1:      
+      case lose:      
       document.querySelector('.gradient').id = 'gradient1';
       document.querySelector('#play-again h2').innerHTML = messageLose;
       document.querySelector('.score h1').innerHTML = parseInt(sessionStorage.getItem('score')) - 1;
